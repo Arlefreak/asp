cfdf903c0ad340909ff68c2ea53335eefc64eae9
Arlefreak <arlefreak@gmail.com>
2015-02-10 22:28:39 -0600

    Fixed footer

--- portfolio/context_processors.py
+++ portfolio/context_processors.py
@@ -8,7 +8,6 @@ def menu(request):
         {'name': 'About', 'url': reverse('about')},
         {'name': _('Proyectos'), 'url': reverse('proyects')},
         {'name': _('Noticias'), 'url': reverse('press')},
-        {'name': _('Instagram'), 'url': "http://instagram.com/potro01/"},
     ]}
     for item in menu['menu']:
         if request.path == item['url']:
